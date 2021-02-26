c3159c338a3ffaff038ea24f8d8680344f859265
Klaas <klaas@tinyroar.de>
2021-02-26 19:36:42 +0100

    REFACTOR Split programm into seperate functions

--- David Chat Jammer/Program.cs
+++ David Chat Jammer/Program.cs
@@ -14,52 +14,80 @@ namespace David_Chat_Jammer
 {
     class Program
     {
+        private static string _sConnString;
+        private static string _sLoeschnachricht;
+        private static List<string> _lsIDs;
+        private static string _sComand;
+        private static int _waitTime;
+
         static void Main(string[] args)
         {
-            int iEntfernteEintraege = 0;
-            int iWaitTime = 0;
-            try
-            {
-                iWaitTime = Int32.Parse(ConfigurationManager.AppSettings["WaitTime"]);
-            }
-            catch (Exception)
-            {
-
-                MessageBox.Show("Fehler beim Lesen der Datei 'David Chat Jammer.exe.config' XML Formatierungsvorgaben werden nicht eingehalten");
-            }
+            int deletedEntries; //Chose english variable names => easier to share, 0 is default value, so does not need to be initialized
+            _waitTime = 0;
+            
+            _waitTime = TryGetWaitTime();
             
             // Connection String anfragen
-            string sConnString = MSSQL_Access.GetConnectionString();
-            string sLoeschnachricht = ConfigurationManager.AppSettings["Message"];
+            _sConnString = MSSQL_Access.GetConnectionString();
+            _sLoeschnachricht = ConfigurationManager.AppSettings["Message"];
+            _lsIDs = new List<string>();
+            _sComand = "SELECT * FROM [David TeamBoards Database].[dbo].[messages]";
 
-            List<string> LsIDs = new List<string>();
+            WriteTitleBanner();
 
-            string sComand = "SELECT * FROM [David TeamBoards Database].[dbo].[messages]";
+          
 
-            Console.WriteLine("------------------------------------------------------------------------");
-            Console.WriteLine("------------------------David Chat Jammer-------------------------------");
-            Console.WriteLine("------------------------------------------------------------------------");
-
-            while (true)
-            {
-
-                Console.WriteLine("Rufe neue Nachrichten ab: ");
-                LsIDs = Get_TableContent.GetDatabaseEntrys(sConnString, sComand, sLoeschnachricht);
-
-                //Console.WriteLine("Entferne Nachrichtentexte:");
-                //Übergibt die Liste der zu Modifizierenden Einträge um diese anzupassen.
-                iEntfernteEintraege= Set_TabelContent.SetDatabaseEntrys(sConnString, LsIDs, sLoeschnachricht);
-                Console.WriteLine(iEntfernteEintraege+" Einträge entfernt. Wartemodus aktiviert");
-
-                //Console.ReadLine();
-                
-                Thread.Sleep(iWaitTime);
-            }
-
+            Thread messageDeleterThread = new Thread(new ThreadStart(DeleteMessageEntries));
+            messageDeleterThread.Start();
 
             //Liste aller Datenbankeinträge, welche den Zielstring enthalten
             
 
+        }
+
+        //is this fine without thread? does this block the main thread? Should this be a seperate thread?
+        private static void DeleteMessageEntries()
+        {
+            List<string> LsIDs;
+            int deletedEntries;
+            while (true)
+            {
+                Console.WriteLine("Rufe neue Nachrichten ab: ");
+                LsIDs = Get_TableContent.GetDatabaseEntrys(_sConnString, _sComand, _sLoeschnachricht);
+
+                //Console.WriteLine("Entferne Nachrichtentexte:");
+                //Übergibt die Liste der zu Modifizierenden Einträge um diese anzupassen.
+                deletedEntries = Set_TabelContent.SetDatabaseEntrys(_sConnString, LsIDs, _sLoeschnachricht);
+                Console.WriteLine(deletedEntries + " Einträge entfernt. Wartemodus aktiviert");
+
+                //Console.ReadLine();
+
+                Thread.Sleep(_waitTime);
+            }
+        }
+
+
+        private static void WriteTitleBanner()
+        {
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine("------------------------David Chat Jammer-------------------------------");
+            Console.WriteLine("------------------------------------------------------------------------");
+        }
+
+        private static int TryGetWaitTime()
+        {
+            int waitTime = 0; 
+            try
+            {
+                waitTime = Int32.Parse(ConfigurationManager.AppSettings["WaitTime"]);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    "Fehler beim Lesen der Datei 'David Chat Jammer.exe.config' XML Formatierungsvorgaben werden nicht eingehalten");
+            }
+
+            return waitTime;
         }
     }
 }
